3fc3bf4da3a63745e9734a3bbe8391ae5ab89aa8
FarazAhmad-117 <farazahmad31048@gmail.com>
2025-01-22 00:52:22 +0500

    some work done

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h2 className="text-blue-500 text-2xl">Hello ChitChat!</h2>
-    </div>
-  );
-}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -9,6 +10,10 @@ const config: Config = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: "class",
+    })
+  ],
 };
 export default config;
